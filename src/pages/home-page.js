88968eadbf3196004d50cf7a8c6cc9c9558c9592
Ann Marie Garrett <amgarrett09@gmail.com>
2018-05-21 19:23:28 -0400

    added a test home-page file

--- src/pages/home-page.js
+++ src/pages/home-page.js
@@ -2,3 +2,14 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 // insert home page code
+
+const homePage = () => (
+  <div>
+    <h1>Homepage</h1>
+    <p>This is homepage stuff.</p>
+    
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default homePage
